c7594875f8fbcbdaff4c36ecbfbebf3bce823731
logan-inspace <logan.jo@inspacecorp.com>
2026-05-31 00:20:10 +0900

    refactor(repository): move extension functions inside companion objects
    Move getById extension functions from top-level to companion objects within
    their respective repository interfaces for better encapsulation and code
    organization.
    
    - InventoryRepository.getById moved to companion object
    - OrderRepository.getById moved to companion object
    - UserRepository.getById moved to companion object

--- domain/src/main/kotlin/com/ark/base/order/OrderRepository.kt
+++ domain/src/main/kotlin/com/ark/base/order/OrderRepository.kt
@@ -4,7 +4,10 @@ import com.ark.base.common.BaseException
 import com.ark.base.common.ErrorCode
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface OrderRepository : JpaRepository<Order, Long>
+interface OrderRepository : JpaRepository<Order, Long> {
+    companion object {
+        fun OrderRepository.getById(id: Long): Order =
+            findById(id).orElseThrow { BaseException(ErrorCode.ORDER_NOT_FOUND) }
 
-fun OrderRepository.getById(id: Long): Order =
-    findById(id).orElseThrow { BaseException(ErrorCode.ORDER_NOT_FOUND) }
+    }
+}
--- domain/src/main/kotlin/com/ark/base/user/UserRepository.kt
+++ domain/src/main/kotlin/com/ark/base/user/UserRepository.kt
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository : JpaRepository<User, Long> {
     fun findByEmail(email: String): User?
-}
 
-fun UserRepository.getById(id: Long): User =
-    findById(id).orElseThrow { BaseException(ErrorCode.USER_NOT_FOUND) }
+    companion object {
+        fun UserRepository.getById(id: Long): User =
+            findById(id).orElseThrow { BaseException(ErrorCode.USER_NOT_FOUND) }
+    }
+}
